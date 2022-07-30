8e3d4f50027c9716085bfee648e6a60d7245b2f1
SloaneY <tpdms717@gmail.com>
2022-07-30 15:45:18 +0900

    Create nweet and add to Firestore DB

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
@@ -12,6 +13,7 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig)
 const authService = getAuth(app)
+export const dbService = getFirestore(app)
 
-export const firebaseInstance = getAuth(app);
+export const firebaseInstance = getAuth(app)
 export default authService
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { dbService } from '../Firebase';
+import { collection, addDoc } from 'firebase/firestore';
 
 const Home = () => {
+    const [nweet, setNweet] = useState('');
+    const onSubmit = async (e) => {
+        e.preventDefault();
+        await addDoc(collection(dbService, 'nweets'), {
+            nweet,
+            createdAt: Date.now()
+        });
+        setNweet('');
+    }
+    const onChange = (e) => {
+        const { target: { value } } = e;
+        setNweet(value);
+    }
+
     return (
-        <div>Home</div>
+        <div>
+            <form onSubmit={onSubmit}>
+                <input value={nweet} onChange={onChange} type="text" placeholder="What's on your mind?" maxLength={120} />
+                <input type="submit" value='Nweet' />
+            </form>
+        </div>
     )
 }
 
