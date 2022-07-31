da048e0487fba82319313deff46a0d58a3b5bcf0
SloaneY <tpdms717@gmail.com>
2022-07-31 16:56:38 +0900

    How to create and filter query

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useHistory } from 'react-router';
-import authService from '../Firebase';
+import authService, { dbService } from '../Firebase';
+import { collection, query, where, getDocs, orderBy } from 'firebase/firestore';
 
-const Profile = () => {
+const Profile = ({ userObj }) => {
   const history = useHistory();
   const onLogOutClick = () => {
     authService.signOut();
     history.push('/');
   }
+  const getMyNweets = async () => {
+    const q = query(collection(dbService, 'nweets'), where('creatorId', '==', userObj.uid), orderBy('createdAt'));
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+      console.log(doc.id, '==', doc.data())
+    });
+
+  }
+  useEffect(() => {
+    getMyNweets()
+  }, [])
 
   return (
     <div>
